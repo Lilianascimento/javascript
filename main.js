6e5b222f1d3bc277c609816c688184a750265e62
Lilianascimento <102737710+Lilianascimento@users.noreply.github.com>
2022-06-02 11:02:16 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+const boasVindas = () => {
+    alert('Boas Vindas a página')
+}
+
+const clique = () => {
+    var b = window.document.getElementById('box');
+    b.innerText = "Chocolate"
+}
+
+const mudaracor = () => {
+    var c = window.document.getElementById('box');
+    c.style.background = 'red'
+}
+
+const mudacor = () => {
+    var d = window.document.getElementById('box');
+    d.style.background = 'yellow'
+}
